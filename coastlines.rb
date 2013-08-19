af16868ec0ca47548f288e569eefe31b2b91b7a7
Bert Spaan <bert.spaan@gmail.com>
2013-08-19 21:32:39 +0200

    Added topojson

--- coastlines.rb
+++ coastlines.rb
@@ -14,22 +14,46 @@ POINTS = <<-SQL
     gid = ?
 SQL
 
+linestrings = []
 JSON.parse(File.open('nearest_points.json').read).each { |gid, nearest_points|  
   nearest_points = nearest_points.map {|i, port_id| [i.to_i - 1, port_id] }.sort
-  puts nearest_points.inspect 
-
+  
+  # Read polygon from database
+  points = DB[POINTS, gid].all
+  
   nearest_points.each_with_index { |(i, port_id), index|
-
     next_index = index + 1
     next_index = 0 if next_index >= nearest_points.length
-    next_i = nearest_points[next_index][0]
-    puts "index: #{index}, next_index: #{next_index}"
-    puts "i: #{i}, next_i: #{next_i}"
     
+    next_i = nearest_points[next_index][0]
+    next_port_id = nearest_points[next_index][1]
+    
+    if next_i > i
+      a = (i..next_i).to_a
+    else
+      a = (i..points.length - 1).to_a + (0..nearest_points[0][0]).to_a
+    end
+    
+    coordinates = a.map { |i| [points[i][:lon], points[i][:lat]]}
+    
+    linestrings << {
+      :type => "Feature",
+      :properties => {
+        :ports => [port_id, next_port_id]
+      },
+      :geometry => {
+        :type => "LineString",
+        :coordinates => coordinates
+      }
+    }
   }
-  #points = DB[POINTS, gid].all
-  #puts points.inspect
-  
-  #puts nearest_points.inspect #Hash[nearest_points.sort]
 }
+
+geojson = {
+  "type" => "FeatureCollection",
+  "features" => linestrings
+}
+
+File.open("coastlines.geojson", 'w') { |file| file.write(JSON.pretty_generate(geojson)) }
+puts "Done..."
 
